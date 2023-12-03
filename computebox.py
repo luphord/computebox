d11c713f6f88923ba0c5f61ef11353d3b55efaf2
luphord <luphord@protonmail.com>
2023-12-03 22:04:35 +0100

    use python API instead of hashbang

--- computebox.py
+++ computebox.py
@@ -1,6 +1,12 @@
-#! /usr/bin/doit -f
+#! /usr/bin/env python3
+
+import doit
 
 
 def task_update_package_index():
     """Update local package index using apt-get update."""
     return {"actions": ["sudo apt-get update"]}
+
+
+if __name__ == "__main__":
+    doit.run(globals())
