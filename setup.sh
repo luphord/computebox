e4eac2546daf6a72c75f2aa5581b8ac1fcbb4787
luphord <luphord@protonmail.com>
2024-01-09 20:34:31 +0100

    install dex .desktop tool

--- setup.sh
+++ setup.sh
@@ -25,7 +25,7 @@ sudo apt-get install -y \
     sbcl slime chezscheme tcc valac \
     sqlitebrowser meld glade spyder geany pspp \
     virt-manager qemu-kvm qemu-utils qemu-block-extra docker.io docker-compose \
-    screenkey impressive redshift \
+    screenkey impressive redshift dex \
     firefox keepass2 \
     arc-theme papirus-icon-theme fonts-powerline cinnamon-desktop-environment \
     gnome-system-monitor gnome-font-viewer gnome-terminal alacritty kitty
