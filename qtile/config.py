998f3c55b1cb37c247367ab833d7a1f3e14ec6c4
luphord <luphord@protonmail.com>
2024-01-14 18:01:59 +0100

    increase focussed border width

--- qtile/config.py
+++ qtile/config.py
@@ -144,7 +144,7 @@ for i in groups:
 
 layouts = [
     layout.Columns(
-        border_focus_stack=["#d75f5f", "#8f3d3d"], border_width=3, margin=[6] * 4
+        border_focus_stack=["#d75f5f", "#8f3d3d"], border_width=6, margin=[6] * 4
     ),
     layout.Max(),
     # Try more layouts by unleashing below layouts.
